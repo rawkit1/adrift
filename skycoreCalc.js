a464f7ae6f8e35fd0449964cfbdfeb2b9df790ff
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-02 14:16:39 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -2,30 +2,17 @@ window.onload  = function() {
 	
 	document.getElementById('casing').addEventListener("click", updateSpeed);
 	document.getElementById('generator').addEventListener("click", updateSpeed);
-	var casing = document.getElementById('casing').options[e.selectedIndex];
-	var gen = document.getElementById('generator').options[e.selectedIndex];
-	//var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69};
-	var genWeight = 0;
-	
+	var casingObj = document.getElementById('casing');
+	var casing = casingObj.options[casingObj.selectedIndex];
+	var genObj = document.getElementById('generator');
+	var gen = casingObj.options[casingObj.selectedIndex];
+	var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69};
 }
 
 
 function updateSpeed(){
-  if (casing.value == 'aluminum') {
-	  genWeight = .26;
-  }
-  else if (casing.value == 'copper') {
-	  genWeight = .50;
-  }
-  else if (casing.value == 'silver') {
-	  genWeight = .55;
-  }
-  else if (casing.value == 'gold') {
-	  genWeight = .69;
-  }
-  else {
-	  genWeight = 0;
-  }
-  alert(genWeight.toString());
+  console.log(casing.value);
+  console.log(gen.value)
+  console.log(dict[gen.value].toString()
   document.getElementById('mass').value = casing.value*100+dict[gen.value]*40;
-			}
+}
