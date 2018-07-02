487f518266eeb8269668a1361b5ad11704a9d20e
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-02 13:17:41 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -1,9 +1,8 @@
-var dict = {'aluminum':.26,'copper':.50,'silver':.55,'gold':.69};
-
 window.onload  = function() {
 	alert("Hello! I am an alert box!!");
 	document.getElementById('casing').addEventListener("click", updateSpeed);
 	document.getElementById('generator').addEventListener("click", updateSpeed);
+	var dict = {'aluminum':.26,'copper':.50,'silver':.55,'gold':.69};
 	var casing = document.getElementById('casing').options[e.selectedIndex];
 	var gen = document.getElementById('generator').options[e.selectedIndex];
 	
