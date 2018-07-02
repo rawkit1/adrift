302452737f537ca38f343b4d2ba28b522cabe727
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-02 13:58:56 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -4,13 +4,29 @@ window.onload  = function() {
 	document.getElementById('generator').addEventListener("click", updateSpeed);
 	var casing = document.getElementById('casing').options[e.selectedIndex];
 	var gen = document.getElementById('generator').options[e.selectedIndex];
-	var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69};
+	//var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69};
+	var genWeight = 0;
 	
 }
 
 
-
 function updateSpeed(){
-  alert(dict[casing.value].toString());
+  if (casing.value == 'aluminum') {
+	  genWeight = .26;
+  }
+  else if (casing.value == 'copper') {
+	  genWeight = .50;
+  }
+  else if (casing.value == 'silver') {
+	  genWeight = .55;
+  }
+  else if (casing.value == 'gold') {
+	  genWeight = .69;
+  }
+  else {
+	  genWeight = 0;
+  }
+  else
+  alert(genWeight.toString());
   document.getElementById('mass').value = casing.value*100+dict[gen.value]*40;
 			}
