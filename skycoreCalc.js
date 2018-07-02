70eddd5d56685de8ad64d18252c525d8b6642040
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-02 13:03:52 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -5,10 +5,12 @@ window.onload=function(){
 	document.getElementById('generator').addEventListener("click", updateSpeed);
 	var casing = document.getElementById('casing').options[e.selectedIndex];
 	var gen = document.getElementById('generator').options[e.selectedIndex];
+	alert("Hello! I am an alert box!!");
 }
 
 
 
 function updateSpeed(){
+  alert("Hello! I am an alert box!!");
   document.getElementById('mass').value = casing.value*100+dict[gen.value]*40;
 			}
