4655fae3c6253abb6f213de818ed0b0fc8c5cc11
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-04 16:25:38 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -1,15 +1,19 @@
 var gen;
 var casing;
 var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69,'n/a':0};
-var dict2 = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69,'n/a':0};
+var dict2 = {'aluminum':6,'copper':7.5,'silver':8,'gold':8.5,'n/a':0};
+var genQ;
 window.onload  = function() {
-	document.getElementById('casing').addEventListener("click", updateSpeed);
-	document.getElementById('generator').addEventListener("click", updateSpeed);
+  document.getElementById('casing').addEventListener("click", updateSpeed);
+  document.getElementById('generator').addEventListener("click", updateSpeed);
+  document.getElementById('genQ').addEventListener("keyup", updateSpeed);
 }
 
 
 function updateSpeed(){
   gen = document.getElementById('generator').value;
   casing = document.getElementById('casing').value;
+  genQ = document.getElementById('genQ').value;
   document.getElementById('mass').value = "Weight: " + casing*100+dict[gen]*40 + "kg";
+  document.getElementById('lift').value = "Lift capacity: " + 1000+dict2[gen]*(10+genQ) + "kg";
 }
