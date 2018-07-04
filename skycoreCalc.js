b6e6f6f3a6d19f68564f0ac3bee6c4cf54822c04
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-04 17:00:04 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -3,6 +3,7 @@ var casing;
 var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69,'n/a':0};
 var dict2 = {'aluminum':6,'copper':7.5,'silver':8,'gold':8.5,'n/a':0};
 var genQ;
+var base;
 var calc1;
 var calc2;
 var calc25;
@@ -11,6 +12,8 @@ window.onload  = function() {
   document.getElementById('casing').addEventListener("click", updateSpeed);
   document.getElementById('generator').addEventListener("click", updateSpeed);
   document.getElementById('genQ').addEventListener("keyup", updateSpeed);
+  document.getElementById('genQ').addEventListener("click", updateSpeed);
+  document.getElementById('type').addEventListener("click", updateSpeed);
 }
 
 
@@ -18,10 +21,11 @@ function updateSpeed(){
   gen = document.getElementById('generator').value;
   casing = document.getElementById('casing').value;
   genQ = parseInt(document.getElementById('genQ').value, 10);
+  base = parseInt(document.getElementById('type').value, 10);
   calc1 = casing*100+dict[gen]*40;
   document.getElementById('mass').value = "Weight: " + calc1.toFixed(2) + "kg";
   calc225 = 10+genQ;
   calc25 = dict2[gen]*calc225;
-  calc2 = 1000 + calc25;
+  calc2 = base + calc25;
   document.getElementById('lift').value = "Lift capacity: " + calc2 + "kg";
 }
