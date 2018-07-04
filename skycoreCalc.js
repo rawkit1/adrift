74459f56e4b2dfe48357ffbdea00c5ffbe97c5e2
rawkit1 <40738327+rawkit1@users.noreply.github.com>
2018-07-04 16:27:49 -0500

    Update skycoreCalc.js

--- skycoreCalc.js
+++ skycoreCalc.js
@@ -3,6 +3,8 @@ var casing;
 var dict = {'aluminum':0.26,'copper':0.50,'silver':0.55,'gold':0.69,'n/a':0};
 var dict2 = {'aluminum':6,'copper':7.5,'silver':8,'gold':8.5,'n/a':0};
 var genQ;
+var calc1;
+var calc2;
 window.onload  = function() {
   document.getElementById('casing').addEventListener("click", updateSpeed);
   document.getElementById('generator').addEventListener("click", updateSpeed);
@@ -14,6 +16,8 @@ function updateSpeed(){
   gen = document.getElementById('generator').value;
   casing = document.getElementById('casing').value;
   genQ = document.getElementById('genQ').value;
-  document.getElementById('mass').value = "Weight: " + casing*100+dict[gen]*40 + "kg";
-  document.getElementById('lift').value = "Lift capacity: " + 1000+dict2[gen]*(10+genQ) + "kg";
+  calc1 = casing*100+dict[gen]*40;
+  document.getElementById('mass').value = "Weight: " + calc1 + "kg";
+  calc2 = 1000+dict2[gen]*(10+genQ);
+  document.getElementById('lift').value = "Lift capacity: " + calc2 + "kg";
 }
